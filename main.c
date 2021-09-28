f9fcacb79c4f839781c0a2fbf31552ba6e3da2a1
dfilipski <danny.filipski@gmail.com>
2021-09-27 22:10:43 -0400

    Add framework for accepting user input

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 
+void getUserChoice(char* c);
+int choiceValid(char* c);
+void clearKeyboardBuffer(void);
+
 int main() {
-    printf("Hello, World!\n");
+    int energyMax = 100;
+    int energy = energyMax;
+    char c = '0'; //initialize c to an irrelevant value.
+
+    while (c != 'q') {
+        printf("Current Energy: %d\n", energy);
+        getUserChoice(&c);
+    }
     return 0;
 }
+
+void getUserChoice(char* c) {
+    char tempChar;
+
+    do {
+        printf("What would you like to do:\n"
+               "[w]ithdraw\n"
+               "[d]eposit\n"
+               "[q]uit\n");
+
+        scanf("%c", &tempChar);
+        clearKeyboardBuffer();
+    } while(!choiceValid(&tempChar));
+
+    *c = tempChar;
+}
+
+int choiceValid(char *c) {
+    return (*c == 'q' || *c == 'w' || *c == 'd') ;
+}
+
+void clearKeyboardBuffer(void) {
+    char c;
+    do {
+        scanf("%c", &c);
+    } while (c != '\n');
+}
+

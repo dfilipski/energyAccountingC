9eb7aaf8e8b11adfc922eeb6cb18233bba8d68b6
dfilipski <danny.filipski@gmail.com>
2021-11-02 16:59:33 -0400

    Replace if else with switch, reformat, and add pre/postconditions

--- main.c
+++ main.c
@@ -2,78 +2,104 @@
 #include <stdbool.h>
 #define clear_screen() printf("\e[1;1H\e[2J")
 
+//Precondition: None
+//Postcondition: User will have been prompted to withdraw energy,
+// deposit energy, or quit
 char get_user_choice(void);
+
+//Precondition: none
+//Postcondition: Return true if c is 'w', 'd', or 'q'
 bool choice_valid(char c);
+
+//Precondition: none
+//Postcondition: Keyboard Buffer has been cleared
 void clear_keyboard_buffer(void);
+
+//Precondition: none
+//Postcondition: User has been asked how much energy they have
+// spent. energy - the amount the user specified is returned
 int withdraw(int energy);
+
+//Precondition: None
+//Postcondition: User has been asked how much energy they have
+// gained. energy + the amount the user specified is returned
 int deposit(int energy);
 
-int main() {
-    int energy = 100;
-    char choice = '0'; //initialize choice to an irrelevant value.
+int main()
+{
+	int energy = 100;
+	char choice = '0'; //initialize choice to an irrelevant value.
 
-    while (choice != 'q') {
-        printf("Current Energy: %d\n", energy);
-        choice = get_user_choice();
-        if (choice == 'w')
-            energy = withdraw(energy);
-        else if (choice == 'd')
-            energy = deposit(energy);
-        clear_screen();
-    }
-    return 0;
+	while (choice!='q') {
+		printf("Current Energy: %d\n", energy);
+		choice = get_user_choice();
+		switch (choice) {
+		case 'w': energy = withdraw(energy);
+			break;
+		case 'd': energy = deposit(energy);
+			break;
+		default: break;
+		}
+		clear_screen();
+	}
+	return 0;
 }
 
-char get_user_choice(void) {
-    char temp;
+char get_user_choice(void)
+{
+	char temp;
 
-    do {
-        printf("What would you like to do:\n"
-               "[w]ithdraw\n"
-               "[d]eposit\n"
-               "[q]uit\n");
+	do {
+		printf("What would you like to do:\n"
+			   "[w]ithdraw\n"
+			   "[d]eposit\n"
+			   "[q]uit\n");
 
-        scanf("%c", &temp);
-        clear_keyboard_buffer();
-    } while(!choice_valid(temp));
+		scanf("%c", &temp);
+		clear_keyboard_buffer();
+	} while (!choice_valid(temp));
 
-    return temp;
+	return temp;
 }
 
-bool choice_valid(char c) {
-    return (c == 'q' || c == 'w' || c == 'd') ;
+bool choice_valid(char c)
+{
+	return (c=='q' || c=='w' || c=='d');
 }
 
-void clear_keyboard_buffer(void) {
-    char c;
-    do {
-        scanf("%c", &c);
-    } while (c != '\n');
+void clear_keyboard_buffer(void)
+{
+	char c;
+	do {
+		scanf("%c", &c);
+	} while (c!='\n');
 }
 
-int withdraw(int energy) {
-    int noc, spent_energy;
-    printf("How much energy have you spent? ");
-    noc = scanf("%d", &spent_energy);
-    clear_keyboard_buffer();
+int withdraw(int energy)
+{
+	int noc, spent_energy;
+	printf("How much energy have you spent? ");
+	noc = scanf("%d", &spent_energy);
+	clear_keyboard_buffer();
 
-    while (noc != 1 || spent_energy < 0) {
-        printf("Please enter a positive integer: ");
-        noc = scanf("%d", &spent_energy);
-    }
-    return energy - spent_energy;
+	while (noc!=1 || spent_energy<0) {
+		printf("Please enter a positive integer: ");
+		noc = scanf("%d", &spent_energy);
+	}
+	return energy-spent_energy;
 }
 
-int deposit(int energy) {
-    int noc, gained_energy;
-    printf("How much energy have you regained? ");
-    noc = scanf("%d", &gained_energy);
-    clear_keyboard_buffer();
+int deposit(int energy)
+{
+	int noc, gained_energy;
+	printf("How much energy have you regained? ");
+	noc = scanf("%d", &gained_energy);
+	clear_keyboard_buffer();
 
-    while (noc != 1 || gained_energy < 0) {
-        printf("Please enter a positive integer: ");
-        noc = scanf("%d", &gained_energy);
-    }
-    return energy + gained_energy;
+	while (noc!=1 || gained_energy<0) {
+		printf("Please enter a positive integer: ");
+		noc = scanf("%d", &gained_energy);
+	}
+	return energy+gained_energy;
 }
 
